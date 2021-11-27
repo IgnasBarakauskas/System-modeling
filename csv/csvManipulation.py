8e9344a62cf2d232bd8e7bef7e76cea147e2570a
IgnasBarakauskas <b.ignas05@gmail.com>
2021-11-27 10:54:28 +0100

    added json, csv and initialized xml

---
+++ csv/csvManipulation.py
@@ -0,0 +1,37 @@
+import csv
+
+
+def main():
+    data = readCSV("csv/calculate.csv")
+    calculatedData = []
+    for row in data:
+        calculatedData.append([row[0], row[1], calculateFib(int(row[2]))])
+    writeCSV("csv/answerCalculate.csv", calculatedData)
+
+
+def writeCSV(path, data):
+    f = open(path, "w")
+    for row in data:
+        line = row[0] + "," + row[1] + "," + str(row[2]) + "\n"
+        f.write(line)
+    f.close()
+
+
+def calculateFib(number):
+    if number <= 1:
+        return number
+    else:
+        return (calculateFib(number-1) + calculateFib(number-2))
+
+
+def readCSV(path):
+    with open(path, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        rows = []
+        for row in spamreader:
+            rows.append(row)
+        return rows
+
+
+if(__name__ == "__main__"):
+    main()
